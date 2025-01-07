8b7b3222c52c0730b519d311245c2ad59e2c9c6f
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2025-01-07 19:01:15 +0100

    init

---
+++ Vitesse/Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Hugues Fils Caparos on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
