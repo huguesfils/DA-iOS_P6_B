caf2a2877fab5f325d36ed126c4ea5d0f16b1a1b
Hugues Fils <43337514+huguesfils@users.noreply.github.com>
2025-04-02 00:22:33 +0200

    Update CandidateListViewModel.swift

--- Vitesse/Vitesse/ViewModels/CandidateListViewModel.swift
+++ Vitesse/Vitesse/ViewModels/CandidateListViewModel.swift
@@ -37,27 +37,6 @@ final class CandidateListViewModel {
         }
     }
     
-//    func deleteCandidate(at offsets: IndexSet) async {
-//        for index in offsets {
-//            let candidateToDelete = filteredCandidates[index]
-//            do {
-//                try await networkService.sendVoidRequest(endpoint: .deleteCandidate(candidateId: candidateToDelete.id))
-//                if let idx = candidates.firstIndex(of: candidateToDelete) {
-//                    candidates.remove(at: idx)
-//                }
-//                print("Candidat supprimé: \(candidateToDelete)")
-//            } catch let error as VitesseError {
-//                alertMessage = error.errorMessage
-//                showAlert = true
-//                print("Erreur lors de la suppression: \(error.errorMessage)")
-//            } catch {
-//                alertMessage = VitesseError.candidateDeletionFailed.errorMessage
-//                showAlert = true
-//                print("Erreur inconnue lors de la suppression: \(error.localizedDescription)")
-//            }
-//        }
-//    }
-    
     func deleteCandidate(at offsets: IndexSet) async {
         await withTaskGroup(of: (Candidate, Result<Void, Error>).self) { group in
             for index in offsets {
